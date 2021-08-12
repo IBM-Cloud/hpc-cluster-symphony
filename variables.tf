a2c52a024c930081e0ae5339409f08e985b3cb0b
Takeshi Yoshimura <tyos@jp.ibm.com>
2021-08-12 16:32:36 +0900

    fix cluster_id description

--- variables.tf
+++ variables.tf
@@ -62,7 +62,8 @@ variable "cluster_prefix" {
 
 variable "cluster_id" {
   type        = string
-  description = "Name of the cluster (ClusterID) that you would like to use to create virtual machines in your IBM Cloud account to deploy Spectrum Symphony Cluster. By default, our automation uses a base image with following HPC related packages documented here [Learn more](https://cloud.ibm.com/docs/ibm-spectrum-lsf). If you would like to include your application specific binaries please follow the instructions [Learn more](https://cloud.ibm.com/docs/vpc?topic=vpc-planning-custom-images) to create your own custom image and use that to build the Spectrum Symphony cluster through this offering."
+  default     = "HPCCluster"
+  description = "ID of the cluster that EGO uses. You can check `egosh ego info`"
 }
 
 variable "region" {
